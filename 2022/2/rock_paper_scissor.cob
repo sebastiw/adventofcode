e470f8080aacd15fb46b37879635f501b2981532
Sebastian Weddmark Olsson <visnae@gmail.com>
2022-12-02 15:50:19 +0100

    [2022] Day 2

---
+++ 2022/2/rock_paper_scissor.cob
@@ -0,0 +1,129 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. rock_paper_scissor.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT F-input-FILE
+           ASSIGN TO "input"
+           ORGANIZATION IS LINE SEQUENTIAL.
+       DATA DIVISION.
+       FILE SECTION.
+       FD F-input-FILE.
+       01 FileLine.
+          02 OpponentChoice PIC A(1).
+          02 FILLER PIC Z(1).
+          02 MyChoice PIC A(1).
+
+       WORKING-STORAGE SECTION.
+       01 WS-Points1-TOTAL PIC 9(7) VALUE 0.
+       01 WS-Points2-TOTAL PIC 9(7) VALUE 0.
+
+       01 WS-EOF.
+         02 WS-EOF-BOOL PIC 9(1) VALUE 0.
+
+      *    Points table part 1
+      *    AB  X  Y  Z <- your choice
+      *    A   4  8  3
+      *    B   1  5  9
+      *    C   7  2  6
+      *    ^ opponents choice
+      *
+      *    A/X = Rock
+      *    B/Y = Paper
+      *    C/Z = Scissor
+       01 WS-points1-VALUES.
+         02 PIC 9(3) VALUE "483".
+         02 PIC 9(3) VALUE "159".
+         02 PIC 9(3) VALUE "726".
+
+       01 WS-points1-TABLE REDEFINES WS-points1-VALUES.
+         02 WS-points1-ROWS OCCURS 3 TIMES INDEXED BY I.
+           03 WS-points1-COLS OCCURS 3 TIMES INDEXED BY J.
+             04 WS-points1 PIC 9(1).
+
+      *    Points table part 2
+      *    AB  X  Y  Z <- your choice
+      *    A   3  4  8
+      *    B   1  5  9
+      *    C   2  6  7
+      *    ^ opponents choice
+      *
+      *    A = Rock
+      *    B = Paper
+      *    C = Scissor
+      *    X = Lose
+      *    Y = Draw
+      *    Z = Win
+       01 WS-points2-VALUES.
+         02 PIC 9(3) VALUE "348".
+         02 PIC 9(3) VALUE "159".
+         02 PIC 9(3) VALUE "267".
+
+       01 WS-points2-TABLE REDEFINES WS-points2-VALUES.
+         02 WS-points2-ROWS OCCURS 3 TIMES INDEXED BY I.
+           03 WS-points2-COLS OCCURS 3 TIMES INDEXED BY J.
+             04 WS-points2 PIC 9(1).
+
+       LOCAL-STORAGE SECTION.
+       01 LS-A-Choice PIC 9(1).
+       01 LS-B-Choice PIC 9(1).
+
+       PROCEDURE DIVISION.
+       MAIN-ROUTINE.
+           PERFORM OPEN-FILE-ROUTINE.
+           PERFORM READ-LINE-ROUTINE UNTIL WS-EOF-BOOL = 1.
+           PERFORM CLOSE-FILE-ROUTINE.
+           PERFORM DISPLAY-RESULT-ROUTINE.
+           STOP RUN.
+
+       OPEN-FILE-ROUTINE.
+           OPEN INPUT F-input-FILE.
+       END-ROUTINE.
+
+       READ-LINE-ROUTINE.
+           READ F-input-FILE RECORD
+               AT END SET WS-EOF-BOOL TO 1
+               NOT AT END PERFORM DO-LINE-ROUTINE.
+       END-ROUTINE.
+
+       DO-LINE-ROUTINE.
+           PERFORM SET-OPPONENT-ROUTINE.
+           PERFORM SET-CONTESTANT-ROUTINE.
+           ADD WS-points1(LS-A-Choice, LS-B-Choice)
+               TO WS-Points1-TOTAL.
+           ADD WS-points2(LS-A-Choice, LS-B-Choice)
+               TO WS-Points2-TOTAL.
+       END-ROUTINE.
+
+       SET-OPPONENT-ROUTINE.
+           EVALUATE OpponentChoice
+               WHEN "A"
+                   SET LS-A-Choice TO 1
+               WHEN "B"
+                   SET LS-A-Choice TO 2
+               WHEN "C"
+                   SET LS-A-Choice TO 3
+           END-EVALUATE.
+       END-ROUTINE.
+
+       SET-CONTESTANT-ROUTINE.
+           EVALUATE MyChoice
+               WHEN "X"
+                   SET LS-B-Choice TO 1
+               WHEN "Y"
+                   SET LS-B-Choice TO 2
+               WHEN "Z"
+                   SET LS-B-Choice TO 3
+           END-EVALUATE.
+       END-ROUTINE.
+
+       DISPLAY-RESULT-ROUTINE.
+           DISPLAY "Total points part 1: ", WS-Points1-TOTAL.
+           DISPLAY "Total points part 2: ", WS-Points2-TOTAL.
+       END-ROUTINE.
+
+       CLOSE-FILE-ROUTINE.
+           CLOSE F-input-FILE.
+       END-ROUTINE.
+
+       END PROGRAM rock_paper_scissor.
